e53e5543e932299e93ec99190b30b41eae695ff9
chatlxrdd <londoyt@gmail.com>
2023-09-24 18:05:37 +0200

    Added inf loop and created set of seen items

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const puppeteer = require('puppeteer');
 
     
     const itemsPerPage = '9'; // ile itemkow ma sie pojawic w API (mniej = szybciej działający kod)
-    const catalogIds = '1206'; // id katalogu, łatwo znalezc wystarczy sprawdzic url na stronie
+    const catalogIds = '1242'; // id katalogu, łatwo znalezc wystarczy sprawdzic url na stronie
     const colorIds = ''; // to samo co wyzej
     const brandIds = ''; // jeszcze nie wiem ale sie dowiem ^^
     const sizeIds = ''; // to samo co wyzej
@@ -26,24 +26,39 @@ const puppeteer = require('puppeteer');
     const videoGameRatingIds = ''; //   to samo co wyzej
     const order = 'newest_first'; // sposob w jaki przedmioty sa ukladane w api (newest_first = od najnowszych) (oldest_first = od najstarszych)
 
+    const seenItems = new Set();
+    
+    while (true) {
+        await page.goto(`https://www.vinted.pl/api/v2/catalog/items?per_page=${itemsPerPage}&catalog_ids=${catalogIds}&color_ids=${colorIds}&brand_ids=${brandIds}&size_ids=${sizeIds}&material_ids=${materialIds}&video_game_rating_ids=${videoGameRatingIds}&status_ids&order=${order}`)
 
-    await page.goto(`https://www.vinted.pl/api/v2/catalog/items?per_page=${itemsPerPage}&catalog_ids=${catalogIds}&color_ids=${colorIds}&brand_ids=${brandIds}&size_ids=${sizeIds}&material_ids=${materialIds}&video_game_rating_ids=${videoGameRatingIds}&status_ids&order=${order}`)
+        const data = await page.evaluate(() => {
+            const rawData = JSON.parse(document.querySelector('body').innerText);
+            return rawData.items.slice(0, 1).map(item => ({
+                // tutaj jakie dane chcemy aby program zwrocil, bedzie dodatkowy plik ze wszytskim co ma api do zaoferowania
+                title: item.title,
+                total_item_price: item.total_item_price,
+                brand_title: item.brand_title,
+                url: item.url,
+                photo: {
+                    url: item.photo.url
+                }
+            }));
+        });
 
-    const data = await page.evaluate(() => {
-        const rawData = JSON.parse(document.querySelector('body').innerText);
-        return rawData.items.slice(0,1).map(item => ({
-            // tutaj jakie dane chcemy aby program zwrocil, bedzie dodatkowy plik ze wszytskim co ma api do zaoferowania
-            title: item.title,
-            total_item_price: item.total_item_price,
-            brand_title: item.brand_title,
-            url: item.url,
-            photo: {
-                url: item.photo.url
-            }
-        }));
-    });
+        if (data.length === 0) {
+            break; // No more items to fetch
+        }
 
-    console.log(data);
+        const item = data[0];
+
+        if (!seenItems.has(item.url)) {
+            console.log(item);
+            seenItems.add(item.url);
+        }
+
+        await page.waitForTimeout(5000); // Wait for 5 seconds before fetching the next item
+    }
+
 
     await browser.close()
 })();
