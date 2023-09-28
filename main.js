b7142b0bbbf03b9bb2479befa5071ad35edf815e
Filip Zieliński <filip.zielinski@microsoft.wsei.edu.pl>
2023-09-28 16:54:26 +0200

    xd

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // const puppeteer = require('puppeteer');
 const fs = require('fs');
 const Discord = require('discord.js');
+
 const Config = require('./modules/config.js');
 
 const vintedUrl = Config.vintedLink;
